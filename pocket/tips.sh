840a043235f5dc566e42c4d0a437e012eeddd5ad
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-12-06 18:59:34 +0900

    update modules

---
+++ pocket/tips.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+# -------------------------------------------------- #
+
+# Remove all but one ( or more ) kind of filetype
+# Reference https://unix.stackexchange.com/questions/102427/remove-all-but-one-or-more-kind-of-filetype
+#           https://www.tecmint.com/delete-all-files-in-directory-except-one-few-file-extensions/
+
+# -------------------------------------------------- #
+
+# Execute bash script from URL
+# https://stackoverflow.com/questions/5735666/execute-bash-script-from-url
+
+# -------------------------------------------------- #
