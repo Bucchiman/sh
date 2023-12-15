c3e8fa902308977f8ffee67c4cd07d171c39a94b
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-12-15 16:04:42 +0900

    update modules

--- pocket/tips.sh
+++ pocket/tips.sh
@@ -11,3 +11,48 @@
 # https://stackoverflow.com/questions/5735666/execute-bash-script-from-url
 
 # -------------------------------------------------- #
+# array as arguments
+# Reference https://stackoverflow.com/questions/442012/in-zsh-how-do-i-pass-anonymous-arrays-into-functions
+function i_want_array () {
+    local array_name=$1
+
+    echo "first array element is: " ${(P)${array_name}[1]}
+}
+
+local -a a=(one two three)
+i_want_array "a"
+
+# -------------------------------------------------- #
+# associative array as arguments
+# Reference https://stackoverflow.com/questions/68165452/zsh-return-associative-array-from-function
+createAARR() {
+    # Restrict $name to function scope.
+    local name=$1
+
+    # Delete $1, so $@ becomes the other args.
+    shift
+
+    # Assign elements to array. 
+    set -A "$name" "$@"
+}
+
+typeset -A AARR=()
+createAARR AARR k1 2 k2 4
+typeset -p1 AARR
+#echo ${(k)AARR}
+
+
+# associative array as arguments
+# Reference https://unix.stackexchange.com/questions/506440/pass-associative-array-as-parameter-list-to-script
+to_param_list () {
+    declare -n outlist=$1
+    declare -n inhash=$2
+
+    for param in "${!inhash[@]}"; do
+        outlist+=( "--$param=${inhash[$param]}" )
+    done
+}
+declare -A my_vars=( ["key1"]="value1" ["key2"]="value" )
+
+to_param_list list my_vars
+echo $list[*]
